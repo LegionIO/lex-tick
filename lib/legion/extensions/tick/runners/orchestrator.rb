b5ffdb565e8ebc98f7d26b409bc11aa018f2d8df
Matthew Iverson <matthewdiverson@gmail.com>
2026-03-29 10:03:54 -0500

    swarm: fix for #1 (attempt 1)

--- lib/legion/extensions/tick/runners/orchestrator.rb
+++ lib/legion/extensions/tick/runners/orchestrator.rb
@@ -8,6 +8,10 @@ module Legion
           include Legion::Extensions::Helpers::Lex if Legion::Extensions.const_defined?(:Helpers) &&
                                                       Legion::Extensions::Helpers.const_defined?(:Lex)
 
+          def self.remote_invocable?
+            false
+          end
+
           def execute_tick(signals: [], phase_handlers: {}, **)
             if defined?(Legion::Telemetry::OpenInference)
               state = tick_state
